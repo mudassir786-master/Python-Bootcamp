454461833afcf443084f8a717f2646068a40dda0
mudassir786-master <abuz95548@gmail.com>
2026-08-16 16:49:25 +0530

    ACCEPT CHANGE PLEASE

--- Mini Projects/student_eligibility.py
+++ Mini Projects/student_eligibility.py
@@ -1,5 +1,5 @@
 marks = int(input("Enter your Marks :"))
 attendance = int(input("Enter Attendance (%):"))
 
-eligible = (marks => 35) and (attendance =>75)
+eligible = (marks >= 35) and (attendance >= 75)
 print("Eligible for Exam :", eligible)
